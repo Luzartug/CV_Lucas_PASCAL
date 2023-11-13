953933d6e7de369e1f5c9f12f2e8bad89d81ab9e
Lucas Pascal <lucaspascal@Mac-Lucas.local>
2023-11-13 20:26:15 +0100

    add info

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ picture = current_dir / "assets/profile-pic.png"
 
 # --- GENERAL Setting ---
 PAGE_TITLE = "Digital CV  |  Lucas PASCAL"
-PAGE_ICON = "⬛️"
+PAGE_ICON = "🌊"
 NAME= "Lucas PASCAL :wave:"
 DESCRIPTION = """
 I am in a last year Master in Data and AI, looking for a 6-months final internship as **Data Scientist Intern**.
@@ -26,9 +26,9 @@ PROJECTS = {
     "🏦 Société Générale: Market Risk Variation Algorithm Explanation":"https://wholesale.banking.societegenerale.com/en/solutions/investment-banking/",
     "👀 Streamlit for Netflix Users":"https://github.com/Luzartug",
     "🧠 Psychologue Asistant: Conversational (text and speech) chatbot with emotional analysis":"https://github.com/Luzartug",
-    "⛑️ E-Reputation analysis on TWITTER":"https://github.com/Luzartug"
+    "⛑️ E-Reputation analysis on TWITTER":"https://github.com/Luzartug",
+    "🎧 Podcast Analysis: Summarize of a podcast, keyword search and word cloud graph":"https://github.com/Luzartug"
 }
-
 CERTIFICATION ={
     "AZ-900 certified : Azure Fundamentals":"https://www.credly.com/badges/6603caeb-7e02-4ad3-96be-048357487799/linked_in_profile",
     "AI-900 certified : Azure AI Fundamentals":"https://www.credly.com/badges/fe94c44c-4607-49c3-89af-0022d8972c59/linked_in_profile",
@@ -36,9 +36,6 @@ CERTIFICATION ={
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 st.write("#")
-
-# --- SIDE BAR ---
-#st.sidebar.success("Select a demo above.")
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
@@ -64,64 +61,74 @@ with col2:
     st.write("📩", EMAIL)
 
 # --- SOCIAL LINKS ---
-st.write("#")
+st.write("\n")
 cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     cols[index].write(f"[{platform}]({link})")
 
 # --- EXPERIENCE & QUALIFICATION ---
-st.write("#")
-st.subheader("Experience & Qualifacations")
+st.write("\n")
+st.subheader("Experience & Qualifications")
 st.write(
     """
     - ✔︎ Creating dashboards and predictive models to monitor KPIs for 2 companies
-    - ✔︎ Experience in data collection technology: **Google Analytics** & **SQL database**
+    - ✔︎ Passionate about digital and data: **web analytics, programmatic, Datalake, dataviz, data science**
     - ✔︎ Good understanding of statistical and their respective applications
+    - ✔︎ Strong interest in data project for marketing & business issues
     - ✔︎ Excellent team-player and displaying strong sense of initiative on tasks
     """ 
 )
 
 # --- HARD SKILLS ---
-st.write("#")
-st.subheader("Hard skills")
+st.write("\n")
+st.subheader("Hard & soft skills")
 st.write(
     """
-    - 💻 Programming: **SQL**, **Javascript** and **Python (Scikit-learn, Pandas)**
-    - 📊 Data Visualisation: **PowerBi, Excel, Streamlit**
-    - 📚 Modeling: Logistic regresssion, linear regression, decition trees
-    - 💽 Databases: mySQL, Postgres, MongoDB
+    - 💻 Programming: **SQL**, **Javascript** and **Python**
+    - 📊 Data Visualisation: **Streamlit, Plotly, Matplotlib, Seaborn & PowerBI**
+    - 📚 **Data Science: Machine / Deep Learning, Statistical analysis, computing**
+    - 💽 Databases: mySQL, MongoDB, ElastikSearch
+    """
+)
+st.write(
+    """  
+    - 📡 Good Communication 
+    - 🚀 Consistently aiming for excellence
+    - 📚 Complexe problem solving
+    - 🎙️ Vulgarization
     """
 )
 
 # --- CERTIF ---
-st.write("#")
+st.write("\n")
 st.subheader("Certifications")
 for certif, link in CERTIFICATION.items():
     st.write(f"- ✅ [{certif}]({link})")
 
 
 # --- WORK HISTORY ---
-st.write("#")
+st.write("\n")
 st.subheader("Work History")
 st.write("---")
 
 # --- JOB 1
-st.write("#")
-st.write("🚄", "**Data Analyst Intern | London, UK | Eurostor Int.**")
+st.write("\n")
+st.write("📈", "**Data Analyst Intern | London, UK | Eurostor Int.**")
 st.write("11/2022 - 03/2023")
 st.write(
     """
     - → Involved in the Delta Project, a major European merger in the sector, integrating Eurostar and Thalys to drive synergies 
     and enhance operational efficiency
+    - → Developed data-driven dashboards, improving promotional effectiveness by 15%
     - → Rotation in every team of the IS sales Team (teams in charge of eurostar.com)
+    - → Creating predictive models to monitor KPIs of the company
     - → Working with stakeholders across the Digital Product team
-    - → Creating dashboards and predictive models to monitor KPIs of the company
     """
 )
 
 # --- JOB 2
-st.write("#")
-st.write("🏥", "**Hotline Health Consultant | Orsay, France | French Health Agency (ARS)**")
+st.write("\n")
+st.write("🦠", "**Hotline Health Consultant | Orsay, France | French Health Agency (ARS)**")
 st.write("03/2020 - 06/2020")
 st.write(
     """
@@ -130,9 +137,19 @@ st.write(
     """
 )
 
+# --- JOB 3
+st.write("\n")
+st.write("🧠", "**Science Teacher | Phnom Penh, Cambodia | Royal University of Phnom Penh**")
+st.write("11/2019 - 06/2020")
+st.write(
+    """
+    - → Interned in humanitarian field, designed physics practicals
+    """
+)
+
 
 # ---Projects & Accomplishements
-st.write("#")
+st.write("\n")
 st.subheader("Projects & Accomplishments")
 st.write("---")
 for project, link in PROJECTS.items():
